36cc980918c9694476670324f9757ec5d245961d
SillyFreak <sillyfreak.ck@gmail.com>
2017-09-24 12:35:11 +0200

    add JWT authentication mechanism; right now, JWT is hard-coded into the request handling. There is probably a better way, using the same mechanism that handles token authentication.

--- graphene_jwt/views.py
+++ graphene_jwt/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+from graphene_django.views import GraphQLView
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+
+class JWTGraphQLView(GraphQLView):
+    @method_decorator(ensure_csrf_cookie)
+    def dispatch(self, request, *args, **kwargs):
+        auth = JSONWebTokenAuthentication()
+        user_auth_tuple = auth.authenticate(request)
+        if user_auth_tuple is not None:
+            request.user, request.auth = user_auth_tuple
+        return super(JWTGraphQLView, self).dispatch(request, *args, **kwargs)
